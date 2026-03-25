96a7cf1177bae8ac97afbf7c39977cc5409e5579
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-03-25 23:04:57 +0600

    Added primary toggling functionalities

--- src/components/HomePage/Players/Players.jsx
+++ src/components/HomePage/Players/Players.jsx
@@ -1,12 +1,19 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers/AvailablePlayers';
 
 const Players = ({playerPromise}) => {
     const players = use(playerPromise)
     console.log(players);
+    const[selectedType,setSelectedType] =useState("Available")
     return (
-        <div className='container mx-auto my-6'>
-            <p>players: {players.length}</p>
+        <div className='container mx-auto space-y-4 my-6'>
+            <div className='flex justify-between items-center'>
+                <h2 className='text-3xl font-bold'>Available Players</h2>
+                <div className=''>
+                    <button onClick={()=>setSelectedType("Available")} className={`btn ${selectedType=="Available"?'bg-[#E7FE29]':''} rounded-r-none rounded-l-xl border-none`}>Available</button>
+                    <button onClick={()=>setSelectedType("Selected")} className={`btn ${selectedType=="Selected"?'bg-[#E7FE29]':''} rounded-l-none rounded-r-xl border-none`}>Selected</button>
+                </div>
+            </div>
             <AvailablePlayers players={players}/>
         </div>
     );
